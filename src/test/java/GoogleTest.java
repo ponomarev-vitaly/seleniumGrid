75b8e6a4e79e02c9b1e2ebe0deefb4ecfa3c811d
ponomarev-vitaly <vp211082@gmail.com>
2025-01-06 20:46:29 -0500

    updated GoogleTest.java

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,6 +13,9 @@ public class GoogleTest {
     @Test
     public void HomePageCheck() throws MalformedURLException, URISyntaxException {
         DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        // caps.setPlatform(Platform.);
+
         // URL deprecated, URI
 
         WebDriver driver = new RemoteWebDriver(new URI("http://192.168.1.115:4444").toURL(), caps);
