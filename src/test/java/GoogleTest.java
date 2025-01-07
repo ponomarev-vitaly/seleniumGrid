5b00c228862500bb98ee85493f17844b7debf848
ponomarev-vitaly <vp211082@gmail.com>
2025-01-06 20:38:10 -0500

    updated GoogleTest.java

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 public class GoogleTest {
+
+    @Test
+    public void HomePageCheck() {
+        WebDriver driver = new RemoteWebDriver();
+
+    }
 }
