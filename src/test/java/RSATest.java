770194b33cfe0381ca664ba11aa868e5f1437317
ponomarev-vitaly <vp211082@gmail.com>
2025-01-06 20:51:21 -0500

    created RSATest.java

---
+++ src/test/java/RSATest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class RSATest {
+
+    @Test
+    public void HomePageCheck() throws MalformedURLException, URISyntaxException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        // caps.setPlatform(Platform.);
+        // caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        // caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+
+        // URL deprecated, URI
+
+        WebDriver driver = new RemoteWebDriver(new URI("http://192.168.1.115:4444").toURL(), caps);
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("vitaliy ponomarev");
+        driver.quit();
+
+
+    }
+}
